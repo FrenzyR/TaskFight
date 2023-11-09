b57c6f559b391ffed1f5f6b8b71bf71b17fbe9cd
Rubén Vergara <rubenverrod@gmail.com>
2023-11-09 10:09:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace TaskFight
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            Thread firstThread;            
+            Thread secondThread;
+            
+            firstThread = new Thread(() =>
+            {
+                int i;
+                for (i = 1; i <= 500; i++)
+                {
+                    
+                    
+                }
+                Console.WriteLine();
+            });
+            secondThread = new Thread(() =>
+            {
+                for (int i = 0; i >= -500; i--)
+                {
+                    Console.Write($"\t\t{i}");
+                }
+            });
+            firstThread.Start();
+            secondThread.Start();
+            if (!firstThread.IsAlive) {
+                secondThread = null;
+            }
+            else if (!secondThread.IsAlive)
+            {
+                firstThread = null;
+            }
+            
+        }
+    }
+}
